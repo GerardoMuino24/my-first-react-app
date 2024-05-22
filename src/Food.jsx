5b30a91d3b2eb2eaaa8b5bd94df97853995da378
GerardoMuino24 <gerardomuino@gmail.com>
2024-05-22 16:58:39 -0400

    Clase dia Miercoles 22

---
+++ src/Food.jsx
@@ -0,0 +1,37 @@
+
+// export function Food(){
+
+//     return (
+//         <div>
+//         <div>
+//             <h3> Mi comida favorita es la pizza. </h3>
+//         </div>
+//         <div>
+//             <h2> Es muy sabrosa. </h2>
+//         </div>
+//         </div>
+//     );
+    
+
+// }
+
+export function Food(){
+
+    return (
+        <>
+        <div>
+        <h3> Mi comida favorita es la pizza. </h3>
+        </div>
+        <div>
+        <h6> Es muy sabrosa </h6>
+        </div>
+
+        <div className="container">
+      <svg>
+        <circle cx="25" cy="75" r="20" stroke="green" strokeWidth="2" />
+      </svg>
+    </div>
+        </>
+    );
+    
+}
---
+++ src/Greeting.jsx
@@ -0,0 +1,7 @@
+
+function Greeting(){
+
+    return <h2>Bienvenidos, un saludo, mi nombre es Gerardo</h2>;
+}
+
+export default Greeting;
